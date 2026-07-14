c7361b6bb10907cf9b14cf78893c61456b502aa4
denor1999 <ozhgixinv@list.ru>
2026-07-14 22:03:24 +0300

    added test for login form

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class LoginTests {
+
+    @Test
+    void loginTest(){
+        open("https://qa-guru.github.io/one-page-form/login.html");
+        $("#login-input").setValue("user1");
+        $("#password-input").setValue("password1");
+        $("#submit-button").click();
+        $("#login-form").shouldNotBe(visible);
+    }
 }
